705edb894e0470d4ad9f2da20fdf2ac1e5fb1ff1
Alisson Youssf <youssfbr@gmail.com>
2025-08-20 12:07:14 -0300

    criaando array, validando nome, adicionando no array

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.3
+let amigos = [];
+
+function adicionarAmigo() {  
+
+  //let amigo = document.querySelector('input').value; 
+  let amigo = document.getElementById('amigo').value;
+
+  if (amigo == '') {
+    alert('Por favor, digite um nome.')
+  } else {
+    amigos.push(amigo);
+    document.getElementById('amigo').value = '';
+    alert(amigos);    
+  }
+
+}
