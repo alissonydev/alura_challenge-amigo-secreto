344fef951d6355969c83a75eff7d61aa248d35bd
Alisson Youssf <youssfbr@gmail.com>
2025-08-20 17:50:18 -0300

    Sorteando amigos, verificando se a lista está vazia

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.3
 let amigos = [];
+let resultadoHTML = '';
+const digiteNomeMSG = 'Por favor digite um nome.';
 
 function adicionarAmigo() {  
   
-  let amigo = document.getElementById('amigo').value;    
+  let amigo = document.getElementById('amigo').value;
+  resultadoHTML.innerHTML = '';
 
   if (amigo.trim() === '') {
-    alert('Por favor digite um nome.');
+    alert(digiteNomeMSG);
     return;
   } 
 
   if (amigos.includes(amigo)) {
     alert('Esse nome já consta na lista.');
+    document.querySelector('#amigo').value = ''; 
     amigo.value = '';
     return;
   }
@@ -31,5 +35,17 @@ function mostrarListaAmigos() {
     let listaHTML = document.createElement('li');
     listaHTML.textContent = amigo;
     listaAmigos.appendChild(listaHTML);
+  }
+}
+
+function sortearAmigo() {
+  let qtdAmigos = amigos.length;
+  if (qtdAmigos === 0) {
+    alert(digiteNomeMSG);
+  } else {
+    let indiceAmigo = Math.floor(Math.random() * qtdAmigos);
+    resultadoHTML = document.querySelector('#resultado');
+    listaAmigos.innerHTML = '';
+    resultadoHTML.innerHTML = `O amigo secreto sorteado é: ${amigos[indiceAmigo]}` ;
   }
 }
